927479df308ddf4e416804f6a7d0f935cb228ebd
matthewkrupnik <60237964+matthewkrupnik@users.noreply.github.com>
2025-10-24 21:17:42 -0400

    Initial commit from Specify template

---
+++ SpiceShelf/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SpiceShelf
+//
+//  Created by Matthew Krupnik on 2025-10-24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SpiceShelfTests/SpiceShelfTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpiceShelfTests.swift
+//  SpiceShelfTests
+//
+//  Created by Matthew Krupnik on 2025-10-24.
+//
+
+import Testing
+@testable import SpiceShelf
+
+struct SpiceShelfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
